09c02dc82ab0d31de05f2116bb41fd58b14dc8ee
EdwardB911 <ebukhman@stuy.edu>
2018-02-03 10:26:39 -0500

    wrote fib

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -12,6 +12,34 @@ public class Recursion{
 	}
     }
 
+    public static int fib(int n){
+	if(n < 0){
+	    throw new IllegalArgumentException("n < 0");
+	}
+	if(n == 0){
+	    return 0;
+	}
+	if(n == 1){
+	    return 1;
+	}
+	else{
+	    return fibhelper(n)[0];
+	}
+    }
+
+    public static int[] fibhelper(int n){
+	if (n == 0 || n == 1){
+	    int[] now = {n,0};
+	    return now;
+	}
+	else{
+	    int[] before = fibhelper(n - 1);
+	    int[] now = {before[0] + before[1], before[0]};
+	    return now;
+	}
+    }
+		
+
     public static void main(String[] args){
 	System.out.println(fact(0));
 	System.out.println(fact(3));
