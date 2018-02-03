2946962a0418c5e67bb75c860167d646919cff75
EdwardB911 <ebukhman@stuy.edu>
2018-02-03 11:40:10 -0500

    percentError works

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -23,28 +23,29 @@ public class Recursion{
 	    return 1;
 	}
 	else{
-	    return fibhelper(n)[0];
+	    return fibHelper(n)[0];
 	}
     }
 
-    public static int[] fibhelper(int n){
+    public static int[] fibHelper(int n){
 	if (n == 0 || n == 1){
 	    int[] now = {n,0};
 	    return now;
 	}
 	else{
-	    int[] before = fibhelper(n - 1);
+	    int[] before = fibHelper(n - 1);
 	    int[] now = {before[0] + before[1], before[0]};
 	    return now;
 	}
     }
+
+    public static double percentError(double measured, double accepted){
+	return ((measured - accepted)/accepted);
+    }   
 		
 
     public static void main(String[] args){
-	System.out.println(fib(0));
-	System.out.println(fib(3));
-  	System.out.println(fib(5));
-	System.out.println(fib(40));
-	System.out.println(fact(-1));
+	System.out.println(percentError(5,4));
+	System.out.println(percentError(4,4));
     }
 }
