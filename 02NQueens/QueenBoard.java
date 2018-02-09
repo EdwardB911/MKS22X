423ad5c55d682b1cb08d0419c5509645f9419780
EdwardB911 <ebukhman@stuy.edu>
2018-02-09 15:16:58 -0500

    constructor

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,3 +1,16 @@
 public class QueenBoard{
 
+    private int[][]board;
+
+    public QueenBoard(int size){
+	board = new int[size][size];
+	for(int x = 0; x < size; x++){
+	    for(int y = 0; y < size; y++){
+		board[x][y] = 0;
+	    }
+	}
+    }
+
+    
+
 }
