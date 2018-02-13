de0a230c0863de1e386b16b33f91258a3ffb396a
EdwardB911 <ebukhman@stuy.edu>
2018-02-13 15:21:32 -0500

    constructor

--- 03Knight'sTour/KnightBoard.java
+++ 03Knight'sTour/KnightBoard.java
@@ -1,3 +1,15 @@
 public class KnightBoard{
 
+    public int[] board;
+
+    public KnightBoard(int startingRows,int startingCols){
+	if (startingRows < 0 || startingCols < 0){
+	    throw new IllegalArgumentException();
+	}
+	else{
+	    board = new int[startingRows][startingCols];
+	}
+		
+
+    
 }
