b760f6747dd0f5c9bef17516754fe5f1a15e0574
EdwardB911 <ebukhman@stuy.edu>
2018-02-14 15:30:03 -0500

    toString

--- 03Knight'sTour/KnightBoard.java
+++ 03Knight'sTour/KnightBoard.java
@@ -1,6 +1,6 @@
 public class KnightBoard{
 
-    public int[] board;
+    public int[][] board;
 
     public KnightBoard(int startingRows,int startingCols){
 	if (startingRows < 0 || startingCols < 0){
@@ -10,6 +10,23 @@ public class KnightBoard{
 	    board = new int[startingRows][startingCols];
 	}
     }
+
+    public String toString(){
+	String str = ""
+	for (int row = 0; row < board.length; row++){
+	    for (int col = 0; col < board[].length){
+		if (data[row][col] < 10;){
+		    str = str + "0" + data[row][col];
+		}
+		else{
+		    str = str + data[row][col];
+		}
+	    }
+	    str = str + "\n"
+	}
+    }
+		
+		
 		
 
     
