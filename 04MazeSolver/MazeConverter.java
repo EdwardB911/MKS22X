31e36ff6d4658c15b41ce4d6ff11f2923f639c1f
EdwardB911 <ebukhman@stuy.edu>
2018-02-27 15:31:12 -0500

    still broken

--- 04MazeSolver/MazeConverter.java
+++ 04MazeSolver/MazeConverter.java
@@ -8,14 +8,19 @@ public class MazeConverter{
     public static void main(String[] args) throws FileNotFoundException {
 	File text = new File(args[0]);
 	Scanner inf = new Scanner(text);
-	int rows = 0;
+	int rows = -1;
 	int cols = 0;
 	while(inf.hasNextLine()){
 	    String line = inf.nextLine();
 	    rows = rows + 1;
-	    cols = line.length();
+	    cols = 0;
+	    for(int c = 0; c < line.length();c++){
+		cols = cols + 1;
+	    }
 	    System.out.println(line);
 	}
+	System.out.println(rows);
+	System.out.println(cols);
 	char[][] ary = new char[rows][cols];
 	int x = 0;
 	while(inf.hasNextLine()){
