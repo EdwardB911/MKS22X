14a17f4e51721e4ec8d90a5151db864f0750355e
EdwardB911 <ebukhman@stuy.edu>
2018-03-09 15:27:26 -0500

    fixed compile errors

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -22,19 +22,20 @@ public class USACO{
     public static int[][] depths(int[][] nums, int depth){
 	for(int x = 0; x < nums.length; x++){
 	    for(int y = 0; y < nums[0].length; y++){
-		if(depth - maze[x][y] < 0){
-		    maze[x][y] = (depth - maze[x][y]);
+		if(depth - nums[x][y] < 0){
+		    nums[x][y] = (depth - nums[x][y]);
 		}
 		else{
-		    maze[x][y] = 0;
+		    nums[x][y] = 0;
 		}
 	    }
 	}
+	return nums;
     }
 
     public static int[] parameters(String str){
 	String[] ary = str.split(" ", 0);
-	int[] params = int[ary.length]
+	int[] params = new int[ary.length];
 	    for(int x = 0; x < ary.length; x++){
 		params[x] = Integer.parseInt(ary[x]);
 	    }
