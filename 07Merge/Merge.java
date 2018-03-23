e95a273125d6bcb62348e26a36215a2b544a4c63
EdwardB911 <ebukhman@stuy.edu>
2018-03-23 15:12:29 -0400

    added insertion sort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -36,7 +36,36 @@ public class Merge{
 	msort(data, temp , 0, data.length - 1);
     }
 
-    public static void msort(int[] data, int[] temp, int lo, int hi){
+    public static void insertionSort(int[] ary){
+	insertionSort(ary, 0, ary.length);
+    }
+
+    private static void swap(int[]ary,int a, int b){                    
+	int c =ary[a];
+	ary[a] = ary[b];
+	ary[b] = c;
+    }
+
+    public static void insertionSort(int[] ary, int start, int end){
+	for(int x = start + 1; x < end; x++){
+	    for (int y = x; y > 0; y--){
+		if(ary[y] < ary[y-1]){
+		    swap(ary, y-1, y);
+		}
+	    }
+	}
+    }
+
+    // public static void msort(int[] data, int[] temp, int lo, int hi){
+    // 	if (lo < hi){
+    // 	    int mdpt = (lo + hi)/2;
+    // 	    msort(temp, data, lo, mdpt);
+    // 	    msort(temp, data, mdpt + 1, hi);
+    // 	    merge(temp, data, lo, mdpt, hi);
+    // 	}
+    // }
+
+     public static void msort(int[] data, int[] temp, int lo, int hi){
 	if (lo < hi){
 	    int mdpt = (lo + hi)/2;
 	    msort(temp, data, lo, mdpt);
@@ -44,11 +73,12 @@ public class Merge{
 	    merge(temp, data, lo, mdpt, hi);
 	}
     }
+
 	
 
     public static void main(String[] args){
 	int[] a = {4,2,8,1,6,3};	
-	mergesort(a);
+	insertionSort(a);
 	System.out.println(Arrays.toString(a));
     }
 
