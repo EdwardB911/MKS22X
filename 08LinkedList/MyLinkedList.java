8bda90aee5bd301474f39022e6a19c3289c41b94
EdwardB911 <ebukhman@stuy.edu>
2018-03-28 15:18:50 -0400

    constructor

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -1,6 +1,14 @@
 public class MyLinkedList{
 
     private class Node{
+	private Node next, prev;
+	private int data;
 
+	private Node(int x){
+	    data = x;
+	    next = null;
+	    prev = null;
+	}
+	    
     }
 }
