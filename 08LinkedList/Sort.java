842d5d1128c94b2f4d395659ab33df55f925da9d
EdwardB911 <ebukhman@stuy.edu>
2018-04-15 13:08:43 -0400

    partially debugged

--- 08LinkedList/Sort.java
+++ 08LinkedList/Sort.java
@@ -31,6 +31,9 @@ public class Sort{
 	times = times + 1;
 	for(int x = 1; x <= times; x++){
 	    MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
+	    for(int y = 0; y < 10; y++){
+		buckets[y] = new MyLinkedListImproved<Integer>();
+	    }
 	    for(Integer n : data){
 		int pow = 1;
 		for(int p = 0; p < x; p++){
@@ -38,13 +41,15 @@ public class Sort{
 		}
 	    	int rem = n % pow;
 		int dig = rem / (pow / 10);
+		System.out.println(dig);
 		buckets[dig].add(n);
-		MyLinkedListImproved<Integer> c = new MyLinkedListImproved<Integer>();
-		for(int y = 0; y < 10; y++){
-		    c.extend(buckets[y]);
-		}
-		data = c;
 	    }
+	    MyLinkedListImproved<Integer> c = new MyLinkedListImproved<Integer>();
+	    for(int y = 0; y < 10; y++){
+		c.extend(buckets[y]);
+	    }
+	    data = c;
+	    System.out.println(data);
 	}
     }
 	    
