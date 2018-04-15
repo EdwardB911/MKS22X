f2371bb3d90265e90c363e7a0f9f277961cb39db
EdwardB911 <ebukhman@stuy.edu>
2018-04-15 11:39:08 -0400

    split into pos and neg

--- 08LinkedList/Sort.java
+++ 08LinkedList/Sort.java
@@ -4,7 +4,40 @@ import java.io.*;
 
 public class Sort{
 
+    public static void radixsort(MyLinkedListImproved<Integer> data){
+	MyLinkedListImproved<Integer> pos = new MyLinkedListImproved<Integer>();
+	MyLinkedListImproved<Integer> neg = new MyLinkedListImproved<Integer>();
+	for(Integer n : data){
+	    int index = 0;
+	    if (n < 0){
+		neg.add(n * -1);
+	    }
+	    else{
+		pos.add(n);
+	    }
+	}
+	System.out.println(neg.toString());
+	System.out.println(pos.toString());
 
+    }
+
+    public static void hsort(MyLinkedListImproved<Integer> data){
+	Integer n = data.max();
+	int times = 
+
+    public static void main (String[] args){
+	MyLinkedListImproved<Integer> l = new MyLinkedListImproved<Integer>();
+	l.add(3);
+	l.add(-4);
+	l.add(6);
+	l.add(-3);
+	l.add(1);
+	l.add(2);
+	l.add(8);
+	l.add(-10);
+	System.out.println(l.toString());
+	radixsort(l);
+    }
 
 
 
