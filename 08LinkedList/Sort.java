f1d30607106962dd9a7a8c4ea3da038303ad0113
EdwardB911 <ebukhman@stuy.edu>
2018-04-15 12:44:04 -0400

    need to fix nullpointer

--- 08LinkedList/Sort.java
+++ 08LinkedList/Sort.java
@@ -18,21 +18,45 @@ public class Sort{
 	}
 	System.out.println(neg.toString());
 	System.out.println(pos.toString());
+	hsort(neg);
+	hsort(pos);
+	System.out.println(neg.toString());
+	System.out.println(pos.toString());
 
     }
 
     public static void hsort(MyLinkedListImproved<Integer> data){
-	Integer n = data.max();
-	int times = 
+	Integer a = data.max();
+	int times = (int)(Math.log(a)/Math.log(10));
+	times = times + 1;
+	for(int x = 1; x <= times; x++){
+	    MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
+	    for(Integer n : data){
+		int pow = 1;
+		for(int p = 0; p < x; p++){
+		    pow = pow * 10;
+		}
+	    	int rem = n % pow;
+		int dig = rem / (pow / 10);
+		buckets[dig].add(n);
+		MyLinkedListImproved<Integer> c = new MyLinkedListImproved<Integer>();
+		for(int y = 0; y < 10; y++){
+		    c.extend(buckets[y]);
+		}
+		data = c;
+	    }
+	}
+    }
+	    
 
     public static void main (String[] args){
 	MyLinkedListImproved<Integer> l = new MyLinkedListImproved<Integer>();
 	l.add(3);
-	l.add(-4);
-	l.add(6);
-	l.add(-3);
-	l.add(1);
-	l.add(2);
+	l.add(-400);
+	l.add(60);
+	l.add(-30);
+	l.add(10);
+	l.add(200);
 	l.add(8);
 	l.add(-10);
 	System.out.println(l.toString());
