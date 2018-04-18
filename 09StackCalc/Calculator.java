5484e7b683c98492f582ef83a360bf1765ab32b9
EdwardB911 <ebukhman@stuy.edu>
2018-04-18 18:50:39 -0400

    complete

--- 09StackCalc/Calculator.java
+++ 09StackCalc/Calculator.java
@@ -1,18 +1,50 @@
+import java.util.*;
+import java.io.*;
+
 public class Calculator{
 
     public static double eval(String s){
 	String[] ary = s.split(" ");
-	LinkedList<Integer> l = new LinkedList<>;
+	LinkedList<Double> l = new LinkedList<>();
 	for(String str : ary){
 	    char x = str.charAt(0);
-	    if(x.isDigit()){
-		l.push(str.
-		
+	    if(Character.isDigit(x)){
+		double i = Double.parseDouble(str);
+		l.push(i);
+	    }
+	    else if(x == '+'){
+		double a = l.pop();
+		double b = l.pop();
+		l.push(b + a);
+	    }
+	    else if(x == '-'){
+		double a = l.pop();
+		double b = l.pop();
+		l.push(b - a);
+	    }
+	    else if(x == '*'){
+		double a = l.pop();
+		double b = l.pop();
+		l.push(b * a);
+	    }
+	    else if(x == '/'){
+		double a = l.pop();
+		double b = l.pop();
+		l.push(b / a);
+	    }
+	    else if(x == '%'){
+		double a = l.pop();
+		double b = l.pop();
+		l.push(b % a);
+	    }			   
 	}
+	return l.getFirst();
     }
 
     public static void main(String[] args){
-	eval("11 3 - 4 + 2.5 *");
+	System.out.println(eval("10 2.0 +"));
+	System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+	System.out.println(eval("11 3 - 4 + 2.5 *"));
     }
     
 }
