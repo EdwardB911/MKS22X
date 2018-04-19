47f25a136ab194e2bb2ab7b09d5efb608dfa760b
EdwardB911 <ebukhman@stuy.edu>
2018-04-19 15:29:54 -0400

    new constructor

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -2,11 +2,26 @@ public class MyDeque<E>{
 
     private E[] data;
     private int size;
+    private int front;
+    private int back;
 
     @SuppressWarnings("unchecked")
     public MyDeque(){
 	data =(E[])new Object[10];
 	size = 0;
+	front = 0;
+	back = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public MyDeque(int capacity){
+	if(capacity < 0){
+	    throw new IllegalArgumentException() ;
+	}
+	data =(E[])new Object[capacity];
+	size = 0;
+	front = 0;
+	back = 0;
     }
 
 }
