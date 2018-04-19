da851b532696c9f295f95a71a20fa03ca4554dd4
EdwardB911 <ebukhman@stuy.edu>
2018-04-19 15:16:57 -0400

    size

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,10 +1,12 @@
 public class MyDeque<E>{
 
     private E[] data;
+    private int size;
 
     @SuppressWarnings("unchecked")
     public MyDeque(){
 	data =(E[])new Object[10];
+	size = 0;
     }
 
 }
