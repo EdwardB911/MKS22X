45627d991fd4887c6cd5376c22165624bc997944
EdwardB911 <ebukhman@stuy.edu>
2018-04-29 15:10:57 -0400

    add works

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class MyHeap{
 
     private String[] data;
@@ -20,7 +23,64 @@ public class MyHeap{
 	return size;
     }
 
-    
+    private void resize(){
+	String[] ary = new String[data.length * 2];
+	for(int x = 0; x < ary.length; x++){ 
+ 	    ary[x] = data[x]; 
+ 	}
+	data = ary;
+    }
+
+    private int parent(int x){
+	return ((x - 1)/2);
+    }
+
+    private void swap(int a, int b){                       
+        String c = data[a];                                                          
+        data[a] = data[b];
+	data[b] = c;                                                             
+    }
+
+    public String toString(){
+	return Arrays.toString(data);
+    }
+
+    public void add(String s){
+	if(size == data.length){
+	    resize();
+	}
+	data[size] = s;
+	for(int x = size(); parent(x) >= 0 && (x != 0); x = parent(x)){
+	    if(isMax){
+		if(data[parent(x)].compareTo(data[x]) < 0){
+		    swap(x, parent(x));
+		}
+	    }
+	    else{
+		if(data[parent(x)].compareTo(data[x]) > 0){
+		    swap(x, parent(x));
+		}
+	    }
+	}
+	size = size + 1;
+    }
+
+    public static void main(String[] args){
+	MyHeap max = new MyHeap(true);
+	max.add("apple");
+	max.add("doughnut");
+	max.add("carrot");
+	max.add("egg");
+	System.out.println(max);
+	MyHeap min = new MyHeap(false);
+	min.add("apple");
+	min.add("doughnut");
+	min.add("carrot");
+	min.add("egg");
+	System.out.println(min);
+    }
+		    
+	
 	    
 
 }
