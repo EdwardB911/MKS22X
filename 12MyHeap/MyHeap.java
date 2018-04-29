99474b550208a78a8f0053eba91a787efc8cddf8
EdwardB911 <ebukhman@stuy.edu>
2018-04-29 12:13:30 -0400

    constructors

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,3 +1,26 @@
 public class MyHeap{
 
+    private String[] data;
+    private boolean isMax;
+    private int size;
+
+    public MyHeap(){
+	data = new String[10];
+	isMax = true;
+	size = 0;
+    }
+
+    public MyHeap(boolean b){
+	data = new String[10];
+	isMax = b;
+	size = 0;
+    }
+
+    public int size(){
+	return size;
+    }
+
+    
+	    
+
 }
