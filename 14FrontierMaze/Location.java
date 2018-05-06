735374917f5d6fc66ac859126f5fa07f8501b24e
EdwardB911 <ebukhman@stuy.edu>
2018-05-06 10:56:12 -0400

    location

---
+++ 14FrontierMaze/Location.java
@@ -0,0 +1,25 @@
+public class Location{
+
+    private int row,col;
+
+    private Location previous;
+
+    public Location(int x, int y, Location prev){
+	row = x;
+	col = y;
+	previous = prev;
+    }
+
+    public int getRow(){
+	return row;
+    }
+
+    public int getCol(){
+	return col;
+    }
+
+    public Location getPrevious(){
+	return previous;
+    }
+
+}
