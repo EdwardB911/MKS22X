02bc14e3927c337c54bcd06ecd3b89c36d51d9a0
EdwardB911 <ebukhman@stuy.edu>
2018-05-09 20:53:39 -0400

    added distance

--- 14FrontierMaze/Location.java
+++ 14FrontierMaze/Location.java
@@ -4,16 +4,25 @@ import java.io.*;
 @SuppressWarnings("unchecked")
 
 
-public class Location{
+public class Location implements Comparable<Location>{
 
     private int row,col;
 
     private Location previous;
 
+    private int distance;
+
     public Location(int x, int y, Location prev){
 	row = x;
 	col = y;
 	previous = prev;
+    }
+
+    public Location(int x, int y, Location prev, int dist){
+	row = x;
+	col = y;
+	previous = prev;
+	distance = dist;
     }
 
     public int getRow(){
@@ -24,8 +33,16 @@ public class Location{
 	return col;
     }
 
+    public int getDistance(){
+	return distance;
+    }
+
     public Location getPrevious(){
 	return previous;
+    }
+
+    public int compareTo(Location l){
+	return distance - l.getDistance();
     }
 
 }
